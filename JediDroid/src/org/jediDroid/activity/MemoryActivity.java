50ced578834ac9ec7a5bb7e98e267c6edc730ae7
mgl.olmos <mgl.olmos@0a9c7e29-540a-a145-3b99-e76258f96afd>
2012-02-02 11:13:57 +0000

    Arreglado el bug de reproductor cuando habia puesto las pestañas.
    Implementada el principio de la lógica del memory.

--- JediDroid/src/org/jediDroid/activity/MemoryActivity.java
+++ JediDroid/src/org/jediDroid/activity/MemoryActivity.java
@@ -1,8 +1,65 @@
 package org.jediDroid.activity;
 
+import org.jediDroid.activity.CalculadoraActivity.MyListenerOnClick;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MemoryActivity extends Activity {
+	protected static final String LOG = "JediDroid - MemoryActivity";
 	
+	/** Called when the activity is first created. */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.memory);
+
+
+		/* Defino los listeners */
+		MyListenerOnClick listener = new MyListenerOnClick();
+
+		TextView carta00 = (TextView) findViewById(R.id.carta00);
+		TextView carta01 = (TextView) findViewById(R.id.carta01);
+		TextView carta02 = (TextView) findViewById(R.id.carta02);
+		TextView carta10 = (TextView) findViewById(R.id.carta10);
+		TextView carta11 = (TextView) findViewById(R.id.carta11);
+		TextView carta12 = (TextView) findViewById(R.id.carta12);
+		TextView carta20 = (TextView) findViewById(R.id.carta20);
+		TextView carta21 = (TextView) findViewById(R.id.carta21);
+		TextView carta22 = (TextView) findViewById(R.id.carta22);
+		TextView carta30 = (TextView) findViewById(R.id.carta30);
+		TextView carta31 = (TextView) findViewById(R.id.carta31);
+		TextView carta32 = (TextView) findViewById(R.id.carta32);
+		
+		carta00.setOnClickListener(listener);
+		carta01.setOnClickListener(listener);
+		carta02.setOnClickListener(listener);
+		carta10.setOnClickListener(listener);
+		carta11.setOnClickListener(listener);
+		carta12.setOnClickListener(listener);
+		carta20.setOnClickListener(listener);
+		carta21.setOnClickListener(listener);
+		carta22.setOnClickListener(listener);
+		carta30.setOnClickListener(listener);
+		carta31.setOnClickListener(listener);
+		carta32.setOnClickListener(listener);
+	}
+	
+	/* MyListeners */
+
+	protected class MyListenerOnClick implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			TextView inputResult = (TextView) findViewById(R.id.inputResult);
+
+		}
+
+	}
 
 }
